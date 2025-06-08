0d01b83d63a5142e8682bb6029a556b3931f4792
joelwilner <joelwilner@users.noreply.github.com>
2025-06-08 19:23:12 +0000

    deploy: 12439d5f747c61b11bfab26a780032cd43fc6011

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -796,10 +796,4 @@ var store = [{
         "tags": ["video","layout"],
         "url": "/minimal-mistakes-master/test/layout/uncategorized/2017/01/23/layout-header-video.html",
         "teaser": null
-      },{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
-        "tags": [],
-        "url": "/jekyll/update/2025/05/10/welcome-to-jekyll.html",
-        "teaser": null
       }]
